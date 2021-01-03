40baf46db1c5dd3b8b923a9525c3bd1f17f70acc
Sarah Finck <sarahfinck95@gmail.com>
2021-01-03 10:27:21 -0600

    added questions to JS file and added question line to HTML file

--- code-quiz.js
+++ code-quiz.js
@@ -3,42 +3,36 @@ var questionElement = document.getElementById("questionLine");
 
 //Created array of question objects 
 var questions = [ 
-
 //Question 1 
 {
 question: "Which of the following is the language for describing the presentation of Web pages, including colors, layouts and fonts?",
 choices: ["(a) CSS", "(b) Javascript", "(c) Jquery", "(d) HTML"],
 answer: "(a) CSS"
 }
-
 //Question 2
 {
   question: "What of the following methods is use to repeat a specific block of code a known number of times?",
   choices: ["(a) Function ", "(b) Array", "(c) For Loop", "(d) Math.random()"],
   answer: "(c) For Loop"
 }
-
 //Question 3
 {
   question: "Which of the following was the most popular coding language of 2019?",
   choices: ["(a) Javascript", "(b) Python", "(c) SQL", "(d) HTML"],
   answer: "(b) Python"
 }
-
 //Question 4
 {
-  question: "Which of the following is a distinction from a number array and a ",
-  choices: ["1", "2", etc],
-  answer: "here"
+  question: "All elements of an array are represented from an index which starts at ___.",
+  choices: ["(a) 0", "(b) 1", "(c) -1", "(d) 2"],
+  answer: "(a) 0"
 }
-
 //Question 5
 {
-  question: "enter question here",
-  choices: ["1", "2", etc],
-  answer: "here"
+  question: "Which of the following developers is responsible for implementing visual elements that users see to interact within a web application?",
+  choices: ["(a) Back end web developer", "(b) Front end web developer", "(c) Full stack web developer"],
+  answer: "(b) Front end web developer"
 }
-
 ]
 
 //Start Quiz
