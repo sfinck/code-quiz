0bddc17fc832864d1a07434dad35075881ee75c6
Sarah Finck <sarahfinck95@gmail.com>
2021-01-04 18:27:53 -0600

    changed answers to index values

--- code-quiz.js
+++ code-quiz.js
@@ -11,31 +11,31 @@ var questions = [
 {
 question: "Which of the following is the language for describing the presentation of Web pages, including colors, layouts and fonts?",
 choices: ["(a) CSS", "(b) Javascript", "(c) Jquery", "(d) HTML"],
-answer: "(a) CSS"
+answer: 0,
 }
 //Question 2
 {
   question: "What of the following methods is use to repeat a specific block of code a known number of times?",
   choices: ["(a) Function ", "(b) Array", "(c) For Loop", "(d) Math.random()"],
-  answer: "(c) For Loop"
+  answer: 2,
 }
-//Question 3
+//Question 3 
 {
   question: "Which of the following was the most popular coding language of 2019?",
   choices: ["(a) Javascript", "(b) Python", "(c) SQL", "(d) HTML"],
-  answer: "(b) Python"
+  answer: 1,
 }
 //Question 4
 {
   question: "All elements of an array are represented from an index which starts at ___.",
   choices: ["(a) 0", "(b) 1", "(c) -1", "(d) 2"],
-  answer: "(a) 0"
+  answer: 0,
 }
 //Question 5
 {
   question: "Which of the following developers is responsible for implementing visual elements that users see to interact within a web application?",
   choices: ["(a) Back end web developer", "(b) Front end web developer", "(c) Full stack web developer"],
-  answer: "(b) Front end web developer"
+  answer: 1, 
 }
 ]
 
