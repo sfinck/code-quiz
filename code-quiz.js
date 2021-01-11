7ffe079369eac3c6188d859c98d4e18c93f0b27f
Sarah Finck <sarahfinck95@gmail.com>
2021-01-11 17:20:17 -0600

    recoded time interval element adn change alignment and position of buttons

--- code-quiz.js
+++ code-quiz.js
@@ -8,96 +8,97 @@ var choiceC = document.getElementById("choiceC")
 var choiceD = document.getElementById("choiceD")
 var score = 0;
 var questionNumber = 0;
-var wrongAnswer = 0; 
+var wrongAnswer = 0;
 var finalScores = document.getElementById("finalScores")
+var inputBox = document.getElementById("input");
+var initialsSubmit = document.getElementById("button-addon2");
+var highScores = document.getElementById("highScoresList");
 
 //Created array of question objects 
-var questions = [ 
-//Question 1 
-{
-question: "Which of the following is the language for describing the presentation of Web pages, including colors, layouts and fonts?",
-choices: ["(a) CSS", "(b) Javascript", "(c) Jquery", "(d) HTML"],
-answer: 0,
-},
-//Question 2
-{
-  question: "What of the following methods is use to repeat a specific block of code a known number of times?",
-  choices: ["(a) Function ", "(b) Array", "(c) For Loop", "(d) Math.random()"],
-  answer: 2,
-},
-//Question 3 
-{
-  question: "Which of the following was the most popular coding language of 2019?",
-  choices: ["(a) Javascript", "(b) Python", "(c) SQL", "(d) HTML"],
-  answer: 1,
-},
-//Question 4
-{
-  question: "All elements of an array are represented from an index which starts at ___.",
-  choices: ["(a) 0", "(b) 1", "(c) -1", "(d) 2"],
-  answer: 0,
-},
-//Question 5
-{
-  question: "Which of the following developers is responsible for implementing visual elements that users see to interact within a web application?",
-  choices: ["(a) Back end web developer", "(b) Front end web developer", "(c) Full stack web developer"],
-  answer: 1, 
-}
+var questions = [
+  //Question 1 
+  {
+    question: "Which of the following is the language for describing the presentation of Web pages, including colors, layouts and fonts?",
+    choices: ["(a) CSS", "(b) Javascript", "(c) Jquery", "(d) HTML"],
+    answer: 0,
+  },
+  //Question 2
+  {
+    question: "What of the following methods is use to repeat a specific block of code a known number of times?",
+    choices: ["(a) Function ", "(b) Array", "(c) For Loop", "(d) Math.random()"],
+    answer: 2,
+  },
+  //Question 3 
+  {
+    question: "Which of the following was the most popular coding language of 2019?",
+    choices: ["(a) Javascript", "(b) Python", "(c) SQL", "(d) HTML"],
+    answer: 1,
+  },
+  //Question 4
+  {
+    question: "All elements of an array are represented from an index which starts at ___.",
+    choices: ["(a) 0", "(b) 1", "(c) -1", "(d) 2"],
+    answer: 0,
+  },
+  //Question 5
+  {
+    question: "Which of the following developers is responsible for implementing visual elements that users see to interact within a web application?",
+    choices: ["(a) Back end web developer", "(b) Front end web developer", "(c) Full stack web developer"],
+    answer: 1,
+  }
 ]
 
-//Start Quiz
-startBtn.addEventListener("click", function() {
-genQuiz();
-
-//Timer 
-const startingMinutes = 5;
-let time = startingMinutes * 60;
-
+var time = 60;
 const countdownEl = document.getElementById("countdown");
 
-setInterval(updateCountdown, 1000);
+//Start Quiz
+startBtn.addEventListener("click", function () {
+  genQuiz();
 
-function updateCountdown() {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
+  var timer = setInterval(updateCountdown, 1000);
 
-  seconds = seconds < 10 ? '0' + seconds : seconds;
-
-  countdownEl.innerHTML = `${minutes}:${seconds}`;
-  time--;
-
-  time--;
-  time = time < 0 ? 0 : time; 
-
-  var wrongAnswer = questions[questionNumber].choices;
-  if(userChoice === wrongAnswer){
-    countdownEl--;
+  function updateCountdown() {
+    time--;
+    countdownEl.innerHTML = `: ${time}`;
+    if (time <= 0) {
+      clearInterval(timer);
+      choicesElement.remove();
+      questionElement.innerText = ` Your final score is: ${score}`
+      finalScores.style.display = "block"
+    }
   }
-}
+
 });
 
 //Generates quiz 
-function genQuiz(){
-    choicesElement.style.display = "block";
-    questionElement.innerText = questions[questionNumber].question;
+function genQuiz() {
+  choicesElement.style.display = "block";
+  questionElement.innerText = questions[questionNumber].question;
 
-    choiceA.innerText = questions[0].choices[0];
-    choiceB.innerText = questions[0].choices[1];
-    choiceC.innerText = questions[0].choices[2];
-    choiceD.innerText = questions[0].choices[3];
+  choiceA.innerText = questions[0].choices[0];
+  choiceB.innerText = questions[0].choices[1];
+  choiceC.innerText = questions[0].choices[2];
+  choiceD.innerText = questions[0].choices[3];
 }
 
-function checkAnswer(userChoice){
+function endQuiz() {
+  clearInterval(timer);
+  choicesElement.remove();
+  questionElement.innerText = ` Your final score is: ${score}`
+  finalScores.style.display = "block"
+}
+
+function checkAnswer(userChoice) {
   var correctAnswer = questions[questionNumber].answer;
-  if (userChoice === correctAnswer){
+  if (userChoice === correctAnswer) {
     score++;
   }
   else {
-   wrongAnswer++;
+    time -= 5;
   }
-  };
+};
 
-choicesElement.addEventListener("click", function(event) {
+choicesElement.addEventListener("click", function (event) {
   event.preventDefault();
   var userChoice = event.target.getAttribute("data-index");
   checkAnswer(userChoice);
@@ -109,16 +110,25 @@ choicesElement.addEventListener("click", function(event) {
   choiceB.innerText = questions[questionNumber].choices[1];
   choiceC.innerText = questions[questionNumber].choices[2];
 
-  if (questions[questionNumber].choices.length >=4) {
+  if (questions[questionNumber].choices.length >= 4) {
     choiceD.innerText = questions[questionNumber].choices[3];
   } else {
     choiceD.style.display = "none";
   }
+  if (questionNumber == questions.length - 1) {
+    endQuiz();
+  }
 
-//Display final scores (enter initials)
-  score.addEventListener("click", function() {
+  //Display final scores (enter initials)
+  score.addEventListener("click", function () {
     finalScores.style.display = "block"
-  
+  });
+
+  initialsSubmit.addEventListener("click", function () {
+    finalScores.style.display = "none"
+    var userInitials = inputBox.value.trim
+    var highScore = document.createElement("h4")
+    highScore.innerText = `${userInitials} - ${score}`
   });
 
 }); 
